61536163aa885e7d870c8f55b3cc6f67d2f86925
csendel <csendel4@gmail.com>
2015-05-05 07:12:19 -0600

    Initial Commit

---
+++ CCHS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CCHS
+//
+//  Created by Connor Sendel on 5/5/15.
+//  Copyright (c) 2015 Contripity. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
